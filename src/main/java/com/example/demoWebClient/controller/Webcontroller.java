f6e2f64afe67cd18b30fddfe460549a6435545ac
Anjali Gothi <anjali.gothi@qmetry.com>
2024-01-17 16:37:52 +0530

    added k8sfile

---
+++ src/main/java/com/example/demoWebClient/controller/Webcontroller.java
@@ -0,0 +1,15 @@
+package com.example.demoWebClient.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/anjali/test")
+public class Webcontroller {
+    @GetMapping("/getdata")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("Hello this is k8s testing", HttpStatus.OK);
+    }
+}
