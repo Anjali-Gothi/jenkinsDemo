270527220415bb52a1119e8d034410d5e045ce46
Anjali Gothi <anjali.gothi@qmetry.com>
2024-01-22 16:54:55 +0530

    small change

--- src/main/java/com/example/demoWebClient/controller/Webcontroller.java
+++ src/main/java/com/example/demoWebClient/controller/Webcontroller.java
@@ -4,9 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/anjali/test")
+@RestController
+@RequestMapping("/anjali/test")
 public class Webcontroller {
     @GetMapping("/getdata")
     public ResponseEntity<String> hello() {
